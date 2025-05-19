63e2b432f19ececa84e4c5d651f678db20c45d45
ALEX <alexmunive.0213@gmail.com>
2025-05-19 03:40:44 -0500

    cambio de carpeta a mi imagen

--- src/json/DataCard.js
+++ src/json/DataCard.js
@@ -2,12 +2,12 @@ const data = [
     {
         Title: "Para mí",
         subTitule: "Cotiza tu seguro de salud y agrega familiares si así lo deseas",
-        img: "/public/para-mi.png"
+        img: "../../src/img/para-mi.png"
     },
     {
         Title: "Para alguien más",
         subTitule: "Realizar una cotización para uno de tus familiares o cualquier persona",
-    img: "/public/para-alguien.png"
+       img: "../../src/img/para-alguien.png"
     }
 ]
 
