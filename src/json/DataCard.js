d24dd90a52e8dffb91526b19e1965c72c60e9e3b
ALEX <alexmunive.0213@gmail.com>
2025-05-19 04:05:31 -0500

    cambio de logo

--- src/json/DataCard.js
+++ src/json/DataCard.js
@@ -2,12 +2,12 @@ const data = [
     {
         Title: "Para mí",
         subTitule: "Cotiza tu seguro de salud y agrega familiares si así lo deseas",
-        img: "../../src/img/para-mi.png"
+        number: 1
     },
     {
         Title: "Para alguien más",
         subTitule: "Realizar una cotización para uno de tus familiares o cualquier persona",
-       img: "../../src/img/para-alguien.png"
+       number: 2
     }
 ]
 
--- src/page/quotation/Card.jsx
+++ src/page/quotation/Card.jsx
@@ -2,6 +2,8 @@ import React, { useContext, useState } from 'react'
 import data from "../../json/DataCard"
 import CheckBox from './CheckBox';
 import UserContext from '../../hooks/Context';
+import paraMi from "../../img/para-mi.png"
+import paraAlguien from "../../img/para-alguien.png"
 
 const Card = () => {
   
@@ -22,7 +24,7 @@ const Card = () => {
         >
           <div className="flex flex-row content-center justify-between">
             <div className="flex flex-row content-center md:flex-col">
-              <img className="p-2 md:w-[70px]" src={item?.img} alt="logo" />
+              <img className="p-2 md:w-[70px]" src={item?.number == 1 ? paraMi : paraAlguien } alt="logo" />
               <h1 className="md:pt-0 pb-0 flex justify-center items-center content-center text-xl font-semibold p-2">
                 {item.Title}
               </h1>
